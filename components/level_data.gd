e4ada1d6d96b248857623188065fa953acbf4fda
Jhojan00 <jhojansanchez116@gmail.com>
2026-04-12 07:32:36 -0500

    Improve level switcher capabilities
    This is an early version and it is not scalable, however it makes it
    easier to add new levels.

---
+++ components/level_data.gd
@@ -0,0 +1,5 @@
+class_name LevelData
+extends Resource
+
+@export var scene_uid: String          
+@export var character_path: NodePath  
--- scripts/ui/level_switcher.gd
+++ scripts/ui/level_switcher.gd
@@ -1,59 +1,48 @@
 extends PanelContainer
 
-@onready var switcher: Switcher = $Switcher # HACK -> Change this logic to a switcher :)
-
-@export var containers : Array[PanelContainer] = []
+@onready var switcher: Switcher = $Switcher
+@export var levels: Array[LevelData] = []
+@export var containers: Array[PanelContainer] = []
 @export var unselected: Texture2D
 @export var selected: Texture2D
 
-var selected_idx:int = 0
-var levels = [
-	"uid://bp5rqfllau2l3",
-]
+func _ready() -> void:
+	switcher.content = levels
+	_select(switcher.idx)
 
-func select(container:PanelContainer):
+func _select(idx: int) -> void:
+	var container := containers[idx]
+	var data := levels[idx]
 	container.size_flags_vertical = Control.SIZE_FILL
 	container.get("theme_override_styles/panel").texture = selected
-	
-	var character = container.get_node(container.get_meta("character"))
-	
-	character.play("yes")
-	
-func unselect(container:PanelContainer):
+	get_node(data.character_path).play("yes")
+
+func _unselect(idx: int) -> void:
+	var container := containers[idx]
 	container.size_flags_vertical = Control.SIZE_SHRINK_CENTER
 	container.get("theme_override_styles/panel").texture = unselected
 
-func right():
-	if selected_idx + 1 < len(containers):
-		selected_idx += 1
-		
-		unselect(containers[selected_idx-1])
-		select(containers[selected_idx])
+func right() -> void:
+	_unselect(switcher.idx)
+	switcher.right()
+	_select(switcher.idx)
 
-func left():
-	if selected_idx - 1 > -1:
-		selected_idx -= 1
-			
-		unselect(containers[selected_idx+1])
-		select(containers[selected_idx])
-		
-func _ready() -> void:
-	select(containers[selected_idx])
+func left() -> void:
+	_unselect(switcher.idx)
+	switcher.left()
+	_select(switcher.idx)
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("right"):
 		right()
 		get_viewport().set_input_as_handled()
-			
 	elif event.is_action_pressed("left"):
 		left()
 		get_viewport().set_input_as_handled()
 
-
 func _on_play_pressed() -> void:
-	var level = levels.get(selected_idx)
-	if level:
-		SceneManager.change_scene(level)
+	var data: LevelData = switcher.get_item()
+	if data and not data.scene_uid.is_empty():
+		SceneManager.change_scene(data.scene_uid)
 	else:
-		push_warning("There is no level for idx:%d yet!" % selected_idx)
-	
+		push_warning("No scene UID for level at idx:%d" % switcher.idx)
